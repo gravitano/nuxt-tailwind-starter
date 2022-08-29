85bd5d6d727e6ae1d6232379195bd089b2e04204
Warsono <warsono16694@gmail.com>
2022-08-29 21:30:47 +0700

    refactor: add pages, components and layouts folders

--- app.vue
+++ app.vue
@@ -2,30 +2,10 @@
 useHead({
   title: 'Nuxt 3 + Tailwind CSS',
 });
-
-const count = ref(0);
-
-const increment = () => count.value++;
-const decrement = () => count.value--;
 </script>
 
 <template>
-  <div
-    class="container mx-auto h-screen py-4 text-center grid place-items-center"
-  >
-    <div>
-      <h1 class="text-center text-3xl font-semibold">Nuxt 3 + Tailwind CSS</h1>
-      <p class="text-6xl font-semibold my-20">{{ count }}</p>
-      <div class="space-x-4">
-        <button class="btn" @click="decrement">-</button>
-        <button class="btn" @click="increment">+</button>
-      </div>
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<style scoped>
-.btn {
-  @apply px-4 py-3 rounded-lg border border-gray-500 font-medium w-14 focus:ring select-none;
-}
-</style>
---
+++ components/Counter.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const count = ref(0);
+
+const increment = () => count.value++;
+const decrement = () => count.value--;
+</script>
+
+<template>
+  <div
+    class="container mx-auto h-screen py-4 text-center grid place-items-center"
+  >
+    <div>
+      <h1 class="text-center text-3xl font-semibold">Nuxt 3 + Tailwind CSS</h1>
+      <p class="text-6xl font-semibold my-20">{{ count }}</p>
+      <div class="space-x-4">
+        <button class="btn" @click="decrement">-</button>
+        <button class="btn" @click="increment">+</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  @apply px-4 py-3 rounded-lg border border-gray-500 font-medium w-14 focus:ring select-none;
+}
+</style>
